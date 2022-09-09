4e77b509c4e38d8d1e3d62cce4a6766779fec5de
Clash2453 <spasmilenkov123@gmail.com>
2022-09-09 17:59:37 +0300

    Set up router links

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,33 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <nav class="nav-bar">
+    <ul class="nav-list">
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/calendar" class="nav-link">Calendar</router-link>
+      <router-link to="/inbox" class="nav-link">Inbox</router-link>
+    </ul>
+  </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <router-view></router-view>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.nav-bar {
+  width: calc(100% - 2rem);
+  height: fit-content;
+  padding: 1rem;
+  background: rgb(34, 34, 34);
+  padding: 0.5rem 1rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.nav-list {
+  display: flex;
+  flex-direction: row-reverse;
 }
 </style>
