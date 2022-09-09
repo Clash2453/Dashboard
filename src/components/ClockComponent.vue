50c693dc27c1e6ed7c707d7ba29f87d16a7be64e
Clash2453 <spasmilenkov123@gmail.com>
2022-09-09 20:43:21 +0300

    Create clock and createTask components

---
+++ src/components/ClockComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  width: 25rem;
+  height: 25rem;
+  border-radius: 50%;
+  background-color: var(--gray);
+}
+</style>
---
+++ src/components/CreateTaskComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <input class="text-input" type="text" placeholder="Add new task..." />
+    <div class="additional-options">
+      <button class="create-button">Create</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--gray);
+  padding: 1rem;
+  border-radius: 10px;
+}
+.additional-options {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+}
+.text-input {
+  font-size: 1.5rem;
+  font-family: "Lato", sans-serif;
+  border: none;
+  border-bottom: 3px solid var(--green);
+  background-color: transparent;
+  color: whitesmoke;
+}
+.create-button {
+  background-color: transparent;
+  border: solid 2px var(--green);
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1.2rem;
+  color: white;
+  font-weight: 500;
+  letter-spacing: 0.05rem;
+}
+.additional-options{
+    display: flex;
+    padding: 1rem 0;
+}
+.create-button:hover {
+  background-color: var(--green);
+  color: var(--dark);
+}
+</style>
