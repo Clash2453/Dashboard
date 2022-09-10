ac6e9159e7e16aef0fd9274ee9e44256a46aefbd
Clash2453 <spasmilenkov123@gmail.com>
2022-09-10 21:14:01 +0300

    Make the clock component functional

--- src/components/ClockComponent.vue
+++ src/components/ClockComponent.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <h1 class="time">{{ displayedTime }}</h1>
+  </div>
 </template>
 
 <style scoped>
 .container {
-  width: 25rem;
-  height: 25rem;
+  width: 30rem;
+  height: 30rem;
   border-radius: 50%;
   background-color: var(--gray);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.time {
+  color: whitesmoke;
+  font-size: 4rem;
+  font-family: "Robot", sans-serif;
 }
 </style>
+<script>
+export default {
+  data: function () {
+    return {
+      displayedTime: "",
+    };
+  },
+  methods: {
+    updateTime() {
+      setTimeout(this.updateTime, 1000);
+      let time = new Date();
+      this.displayedTime = time.toLocaleTimeString();
+    },
+  },
+  mounted() {
+    this.updateTime();
+  },
+};
+</script>
