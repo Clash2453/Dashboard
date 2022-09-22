e8b355ce909909b06bb6751c001aee01f4573102
Clash2453 <spasmilenkov123@gmail.com>
2022-09-22 22:02:45 +0300

    Add task task class

---
+++ src/backgroundTasks/taskObject.js
@@ -0,0 +1,9 @@
+class Task {
+  constructor(name, description, due) {
+    this.name = name;
+    this.description = description;
+    this.due = due;
+    this.status = "pending";
+  }
+}
+export default Task;
